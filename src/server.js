99c3cf945b8fd7597bb93c8530b3530d45a2b112
Dianaavelar <104920494+Dianaavelar@users.noreply.github.com>
2022-09-16 15:58:47 -0300

    adicionando

---
+++ src/server.js
@@ -0,0 +1,16 @@
+const http = require('http');
+const servidor  = http.createServer(function (req, resp) {
+    resp.end(`
+        
+            
+                
+            
+            
+                
+ Ultima School 
+
+             
+        
+    `);
+});
+servidor.listen(3000);
